8984eacca1169d38f557a24371da434fa0ee6014
Vebashini Naidoo <Vebashini@rain.co.za>
2019-10-20 21:10:20 +0200

    Updated Readme file with tips

--- R/data.R
+++ R/data.R
@@ -1,22 +1,25 @@
 #' Crime scene report
 #'
-#' Data for the crime scene as per the [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the crime scene as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
 #'
 #' \bold{How to play the game ....}
 #'
-#' Data for the crime scene as per the [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery). This
-#' package makes that data available. \cr\cr
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
 #' There's been a Murder in SQL City!
 #' The Murder Mystery is designed to be both a self-directed lesson to
 #' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
-#' Please read  \bold{game_prompt.pdf} for the details!
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with four variables: \code{date}, \code{type},
 #'   \code{description} and \code{city}.
+#' \describe{
+#' \item{date}{Incident date of form `YYYYMMDD`}
+#' \item{type}{Incident type category - robbery, murder etc.}
+#' \item{description}{More verbose description of crime}
+#' \item{city}{Where did the incident occur?}
+#' }
 #' @examples
 #' # Load a bit of the dataset: crime_scene_report
 #' head(crime_scene_report)
@@ -24,9 +27,17 @@
 
 #' Drivers license
 #'
-#' Data for the drivers license details we have [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the drivers license details as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with nine variables: \code{id}, \code{age},
 #'   \code{height}, \code{eye_color}, \code{hair_color}, \code{gender},
@@ -38,9 +49,17 @@
 
 #' Facebook checkin event
 #'
-#' Data for the facebook checkin events we have [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the facebook checkin events as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with four variables: \code{person_id}, \code{event_id},
 #'   \code{event_name} and \code{date}.
@@ -51,9 +70,17 @@
 
 #' Fit Now checkin details
 #'
-#' Data for the Fit Now checkins we have [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the Fit Now checkins as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with four variables: \code{membership_id}, \code{check_in_date},
 #'   \code{check_in_time} and \code{check_out_time}.
@@ -65,9 +92,17 @@
 
 #' Fit Now member details
 #'
-#' Data for the Fit Now members [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the Fit Now members as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with five variables: \code{id}, \code{person_id},
 #'   \code{name}, \code{membership_start_date} and \code{membership_status}.
@@ -79,9 +114,17 @@
 
 #' Income
 #'
-#' Data for the income information we have [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the income information as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with two variables: \code{ssn} and \code{annual_income}.
 #' @examples
@@ -92,9 +135,17 @@
 
 #' Interview
 #'
-#' Data for the interviews conducted [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the interviews conducted as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with two variables: \code{person_id} and \code{transcript}.
 #' @examples
@@ -105,9 +156,17 @@
 
 #' Person
 #'
-#' Data for the drivers license details we have [SQL Murder](http://mystery.knightlab.com/walkthrough.html)
-#' and [here] (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
-#' of the SQL game [here](https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#' Data for the person details as per the SQL Murder (http://mystery.knightlab.com/walkthrough.html)
+#' and here (https://github.com/NUKnightLab/sql-mysteries). Simon Willison made an online version
+#' of the SQL game (https://sql-murder-mystery.datasette.io/sql-murder-mystery).
+#'
+#' \bold{How to play the game ....}
+#'
+#' This package makes the data of the SQL Murder Mystery available in R. \cr\cr
+#' There's been a Murder in SQL City!
+#' The Murder Mystery is designed to be both a self-directed lesson to
+#' learn R concepts and is also a fun game for experienced R users to solve an intriguing crime.
+#' Please read the \bold{Readme file} for the details!
 #'
 #' @format A data set with six variables: \code{id}, \code{name},
 #'   \code{license_id}, \code{address_number}, \code{address_street_name} and \code{ssn}.
